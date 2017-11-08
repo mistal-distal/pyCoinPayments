d2001366dca45d8cd3328b6cbdcf41a0bae6a24f
James HU <me@jamesatchue.com>
2017-11-07 23:41:09 -0500

    initial version of client

---
+++ pyCoinPayments.py
@@ -0,0 +1,220 @@
+import urllib
+import urllib2
+import hmac
+import hashlib
+import json
+from collections import namedtuple
+
+
+def _json_hook(d): 
+    return namedtuple('X', d.keys())(*d.values())
+
+def pObject(data): 
+    return json.loads(data, object_hook=_json_hook).result
+
+
+class CryptoPayments():
+
+        
+    url = 'https://www.coinpayments.net/api.php'
+    
+
+    def __init__(self, publicKey, privateKey, ipn_url):
+        self.publicKey = publicKey
+        self.privateKey = privateKey
+        self.ipn_url = ipn_url
+        self.format = 'json'
+        self.version = 1
+
+    def createHmac(self, **params):
+        """ Generate an HMAC based upon the url arguments/parameters
+            
+            We generate the encoded url here and return it to Request because
+            the hmac on both sides depends upon the order of the parameters, any
+            change in the order and the hmacs wouldn't match
+        """
+        encoded = urllib.urlencode(params)
+        return encoded, hmac.new(self.privateKey, encoded, hashlib.sha512).hexdigest()
+
+    def Request(self, request_method, **params):
+        """The basic request that all API calls use
+
+            the parameters are joined in the actual api methods so the parameter
+            strings can be passed and merged inside those methods instead of the 
+            request method
+        """
+        encoded, sig = self.createHmac(**params)
+
+        headers = {'hmac': sig}
+
+        if request_method == 'get':
+            req = urllib2.Request(url, headers=headers)
+        elif request_method == 'post':
+            headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            req = urllib2.Request(self.url, data=encoded, headers=headers)
+
+        try:
+            response      = urllib2.urlopen(req)
+            status_code   = response.getcode()
+            response_body = response.read()
+            print response_body
+        except urllib2.HTTPError as e:
+            status_code   = e.getcode()
+            response_body = e.read()
+
+        return pObject(response_body)
+
+
+
+    def createTransaction(self, params={}):
+        """ Creates a transaction to give to the purchaser
+            https://www.coinpayments.net/apidoc-create-transaction
+        """
+        params.update({'cmd':'create_transaction',
+                       'ipn_url':self.ipn_url,
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format}) 
+        return self.Request('post', **params)
+
+
+    
+    def getBasicInfo(self, params={}):
+        """Gets merchant info based on API key (callee)
+           https://www.coinpayments.net/apidoc-get-basic-info
+        """
+        params.update({'cmd':'get_basic_info',
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format})
+        return self.Request('post', **params)
+
+
+
+    def rates(self, params={}):
+        """Gets current rates for currencies
+           https://www.coinpayments.net/apidoc-rates 
+        """
+        params.update({'cmd':'rates',
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format})
+        return self.Request('post', **params)
+
+
+
+    def balances(self, params={}):
+        """Get current wallet balances
+            https://www.coinpayments.net/apidoc-balances
+        """
+        params.update({'cmd':'balances',
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format})
+        return self.Request('post', **params)
+
+
+    def getDepositAddress(self, params={}):
+        """Get address for personal deposit use
+           https://www.coinpayments.net/apidoc-get-deposit-address
+        """
+        params.update({'cmd':'get_deposit_address',
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format})
+        return self.Request('post', **params)
+
+
+    def getCallbackAddress(self, params={}):
+        """Get a callback address to recieve info about address status
+           https://www.coinpayments.net/apidoc-get-callback-address 
+        """
+        params.update({'cmd':'get_callback_address',
+                       'ipn_url':self.ipn_url,
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format})
+        return self.Request('post', **params)
+
+    def createTransfer(self, params={}):
+        """Not really sure why this function exists to be honest, it transfers
+            coins from your addresses to another account on coinpayments using
+            merchant ID
+           https://www.coinpayments.net/apidoc-create-transfer
+        """
+        params.update({'cmd':'create_transfer',
+                       'key':self.publicKey,
+                       'version': self.version,
+                       'format': self.format})
+        return self.Request('post', **params)
+
+    def createWithdrawal(self, params={}):
+        """Withdraw or masswithdraw(NOT RECOMMENDED) coins to a specified address,
+        optionally set a IPN when complete.
+            https://www.coinpayments.net/apidoc-create-withdrawal
+        """
+        params.update({'cmd':'create_withdrawal',
+                        'key':self.publicKey,
+                        'version': self.version,
+                        'format': self.format})
+        return self.Request('post', **params)
+
+
+    
+    def convertCoins(self, params={}):
+        """Convert your balances from one currency to another
+            https://www.coinpayments.net/apidoc-convert 
+        """
+        params.update({'cmd':'convert',
+                        'key':self.publicKey,
+                        'version': self.version,
+                        'format': self.format})
+        return self.Request('post', **params)
+
+    def getWithdrawalHistory(self, params={}):
+        """Get list of recent withdrawals (1-100max)
+            https://www.coinpayments.net/apidoc-get-withdrawal-history 
+        """
+        params.update({'cmd':'get_withdrawal_history',
+                        'key':self.publicKey,
+                        'version': self.version,
+                        'format': self.format})
+        return self.Request('post', **params)
+
+    def getWithdrawalInfo(self, params={}):
+        """Get information about a specific withdrawal based on withdrawal ID
+            https://www.coinpayments.net/apidoc-get-withdrawal-info
+        """
+        params.update({'cmd':'get_withdrawal_info',
+                        'key':self.publicKey,
+                        'version': self.version,
+                        'format': self.format})
+        return self.Request('post', **params)
+
+
+    def getConversionInfo(self, params={}):
+        """Get information about a specific withdrawal based on withdrawal ID
+            https://www.coinpayments.net/apidoc-get-conversion-info
+        """
+        params.update({'cmd':'get_conversion_info',
+                        'key':self.publicKey,
+                        'version': self.version,
+                        'format': self.format})
+        return self.Request('post', **params)
+
+
+
+
+    def validate_mac(uuid, price, currency, test_hash):
+        to_check = YOUR_API_KEY + '_' + uuid + '_' + str(int(price*100)) + currency
+        computed_hash = hashlib.sha256(to_check).hexdigest()
+        return (computed_hash == test_hash)
+
+
+
+
+
+
+
+
+
